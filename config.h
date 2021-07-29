be5f78bca3af00459fd3d76d1e6a9b0c4b5a8d9c
Kyle Se <kyle@skynet.localdomain>
2021-07-29 21:08:14 +1000

    commit

--- config.h
+++ config.h
@@ -266,7 +266,7 @@ static MouseShortcut maltshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
-#define TERMMOD (ControlMask|ShiftMask)
+#define TERMMOD (AltMask|ShiftMask)
 
 static char *openurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -o", "externalpipe", NULL };
 static char *copyurlcmd[] = { "/bin/sh", "-c", "st-urlhandler -c", "externalpipe", NULL };
